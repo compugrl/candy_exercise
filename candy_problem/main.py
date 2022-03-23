ee829d07267757a713a120c5528e2309276b2d8e
Shayla L <88570826+compugrl@users.noreply.github.com>
2022-03-22 17:35:08 -0700

    added functions and updated tests

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -36,16 +36,72 @@ write tests to handle edge cases.
 
 #1
 def get_friends_favorite_candy_count(favorites):
-    pass
+    candy_count = 0
+    candy_counts = {}
+
+    for friend in favorites:
+        for candy in friend[1]:
+            candy_count = candy_counts.get(candy,0)
+            candy_counts[candy] = candy_count + 1
+
+    return candy_counts 
+
 
 #2
 def create_new_candy_data_structure(data):
-    pass 
+    candy_dict = {}
+    friend_name = []
+    candy = ""
+
+    for friend in data:
+        friend_name = friend[0]
+
+        for i in range(len(friend[1])):
+            candy = friend[1][i]
+
+            if candy not in candy_dict:
+                candy_dict.update({candy:[friend_name]})
+            else:
+                candy_dict[candy].append(friend_name)
+            
+    return candy_dict 
 
 #3
 def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+    friend_likes = create_new_candy_data_structure(data)
+    candy_tuple = tuple(friend_likes[candy_name])
+
+    return candy_tuple
 
 #4
 def create_candy_set(data):
-    pass 
+    candy_dict = create_new_candy_data_structure(data)
+    candies = set()
+    candy=""
+
+    for candy in candy_dict:
+        candies.add(candy)
+
+    return candies
+
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+# result1 = get_friends_favorite_candy_count(friend_favorites)
+# print(f"{result1=}\n")
+
+# result2 = create_new_candy_data_structure(friend_favorites)
+# print(f"{result2=}\n")
+
+# result3 = get_friends_who_like_specific_candy(friend_favorites, "lollipop")
+# print(f"{result3=}\n")
+
+# result4 = create_candy_set(friend_favorites)
+# print(f"{result4=}\n")
+
+lollipop_friends = get_friends_who_like_specific_candy(friend_favorites, "lollipop")
+print(lollipop_friends)
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -32,4 +32,55 @@ def test_create_candy_data_structure_values():
     new_candy_data = create_new_candy_data_structure(friend_favorites)
     
     # Assert
+    assert new_candy_data["milky way"] == ['Bob', 'Arlene']
+
+def test_get_count_returns_correct_number():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    candy_count = get_friends_favorite_candy_count(friend_favorites)
     
+    # Assert
+    assert candy_count["laffy taffy"] == 3
+
+def test_get_specific_candy_friends_returns_correct_value():
+
+    # Arrange
+    friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+    ]
+
+    # Act
+    lollipop_friends = get_friends_who_like_specific_candy(friend_favorites, "lollipop")
+    
+    # Assert
+    assert lollipop_friends == ('Sally', 'Bob')
+    assert type(lollipop_friends) == tuple
+
+def test_create_candy_set_returns_set():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    candy_set = create_candy_set(friend_favorites)
+    
+    # Assert
+    assert len(candy_set) == 8
+    assert type(candy_set) == set
+    assert "lollipop" in candy_set
